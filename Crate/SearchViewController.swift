b30474cc7d99cc00bd1ac63d61f9a1ca8a20b67c
ashleyg101 <ashleygong2000@yahoo.com>
2021-11-30 16:46:52 -0500

    navigation flow (screens, tab bar controllers, navigation controllers, view controller, table view controllers), search bar

---
+++ Crate/SearchViewController.swift
@@ -0,0 +1,53 @@
+//
+//  searchViewController.swift
+//  Crate
+//
+//  Created by Ashley Gong on 11/28/21.
+//
+
+import UIKit
+
+class searchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
+//        "https://musicbrainz.org/ws/2/recording?query=%22we%20will%20rock%20you%22%20AND%20arid:0383dadf-2a4e-4d10-a46a-e9e041da8eb3"
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    print(dataDictionary)
+             }
+        }
+        task.resume()
+        //tableView.contentInset = UIEdgeInsets(top: 56, left: 0, bottom: 0, right: 0)
+    }
+}
+
+// MARK: - Search Bar Delegate
+extension searchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+    }
+}
+
+// MARK: - Table View Delegate
+extension searchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
